b040c69f590a2a9e6693f1a108320536010348c1
Апарин Александр <mir-uspeha@mail.ru>
2018-12-08 21:23:42 +0300

    homepage is done

--- src/main/control/mainListeners.js
+++ src/main/control/mainListeners.js
@@ -15,6 +15,16 @@ export default function mainListeners() {
         if (action === 'anna-ph') document.body.appendChild(popup("Мой телефон: +7 920 670 1712.<br> Звоните в рабочие дни с 10 до 18 МСК"));
     });
     writeUs.onclick = (() => {
-        document.body.appendChild(popup("", 300));
+        document.body.appendChild(popup(`
+        <p><b>Александр</b> <br> (сайты, программирование):
+        <a href="https://t.me/aaaparin" target="_black">Телеграм</a>, <a href="https://vk.com/aaaparin" target="_black">ВКонтакте</a>, <a href="https://join.skype.com/invite/gAH0dYmscpwf" target="_black">Skype</a><br>
+        Почта: alex@efeto.ru. <br>
+        Телефон: +7 920 671 7000. Рабочие дни с 10 до 18 МСК</p>
+            
+        <p><b>Анна</b> (копирайтинг,соц. сети): <br>
+        Viber, WhatsApp, Телеграмм, телефон: +7 920 670 1712. <br>
+        Почта: anna@efeto.ru. <a href="https://vk.com/id85981752" target="_black">ВКонтакте</a><br>
+        Пишите и звоните в рабочие дни с 10 до 18 МСК.</p>
+        `, 360));
     });
 }
