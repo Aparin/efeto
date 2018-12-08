3cbb672c5651318bc302e66e6c6fbbbb70a07868
Апарин Александр <mir-uspeha@mail.ru>
2018-12-08 16:50:10 +0300

    made block of contacts

--- src/main/control/mainListeners.js
+++ src/main/control/mainListeners.js
@@ -8,41 +8,11 @@ export default function mainListeners() {
         if (action === 'alex-tel') window.open('https://t.me/aaaparin', '_blank');
         if (action === 'alex-sk') window.open('https://join.skype.com/invite/gAH0dYmscpwf', '_blank');
         if (action === 'alex-ph') document.body.appendChild(popup("Мой телефон: +7 920 671 7000. Звоните в рабочие дни с 10 до 18 МСК"));
+
+        if (action === 'anna-wa') document.body.appendChild(popup("Мой Viber и WhatsApp:<br> +7 920 670 1712"));
+        if (action === 'anna-vk') window.open('https://vk.com/id85981752', '_blank');
+        if (action === 'anna-tel') document.body.appendChild(popup("Мой телеграм:<br> +7 920 670 1712"));
+        if (action === 'anna-ph') document.body.appendChild(popup("Мой телефон: +7 920 670 1712.<br> Звоните в рабочие дни с 10 до 18 МСК"));
     });
 
-
-    // const mainContacts = document.getElementById('mainContacts');
-    /*
-        mainContacts.onclick = function(e) {
-            console.log('Click');
-            const target = e.target;
-            const action = target.getAttribute('main-contacts');
-            if (action === 'alex-vk') console.log('Its working');
-            /*
-                    if (action == 'delete') { deleteSticker(target.parentNode.id); }
-                    if (action == 'edit') {
-                        if (document.getElementById(`adress${target.parentNode.id}`)) {
-                            editMapSticker(target.parentNode.id);
-                        } else {
-                            editSticker(target.parentNode.id);
-                        }
-                    }
-                    
-        };*/
-    /*
-        field.addEventListener('mousedown', function(e) {
-            let target = e.target;
-            while (target != document) {
-                const dataAction = target.getAttribute('data-action');
-                const style = target.getAttribute('class');
-                if (dataAction == 'edit' || dataAction == 'delete' || dataAction == 'ok' ||
-                    dataAction == 'close') { return }
-                if (dataAction == 'sticker' && style != 'editSticker' && style != 'editMapSticker') {
-                    if (event.which == 1) moveTheSticker(target.id);
-                    return;
-                }
-                target = target.parentNode;
-            }
-        });
-        */
 }
